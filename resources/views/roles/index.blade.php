1b5885ad219f58313be9cc993abb567682bd9237
Yesho Simon <sepkap123@gmail.com>
2022-05-03 23:24:46 +0300

    show role

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -6,32 +6,53 @@
         <div class="card-body">
           <div class="container" style="padding: 1rem">
             <div class="row">
-                {!! Form::open(array('route' => 'roles.store','method'=>'POST')) !!}
               <div class="col">
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                  <div class="form-group">
-                    <strong>Name:</strong>
-                    {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
-                  </div>
-                </div>
+                <span style="margin-left: 1rem">
+                  <a href="/roles/create" class="btn btn-success" id="user"
+                    ><b>Create New Role</b></a
+                  >
+                </span>
+                <div class="container pt-3">
+                  <h2>Roles</h2>
+                  <table class="table">
+                    <thead class="thead-dark">
+                      <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Role</th>
+                        <th scope="col">Actions</th>
+                      </tr>
+                    </thead>
+                    @foreach ($roles as $post)
+                    <tbody>
+                      <tr>
+                        <th scope="row">{{$post->id}}</th>
+                        <td>{{$post->name}}</td>
 
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                  <div class="form-group">
-                    <strong>Permission:</strong>
-                    <br/>
-                    @foreach($permission as $value)
-                        <label>{{ Form::checkbox('permission[]', $value->id, false, array('class' => 'name')) }}
-                        {{ $value->name }}</label>
-                    <br/>
+                        <td>
+                          <a
+                            href="{{ route('roles.edit',$post->id) }}"
+                            class="btn btn-xs btn-success"
+                            ><i class="fa fa-edit"></i> Edit
+                          </a>
+                          <a
+                            href="/roles/{{$post->id}}"
+                            class="btn btn-xs btn-primary"
+                            ><i class="fa fa-eye"></i> View
+                          </a>
+                          {!! Form::open(['method' => 'DELETE','route' =>
+                          ['roles.destroy',
+                          $post->id],'style'=>'display:inline']) !!} {{
+                          Form::button('<i class="fa fa-trash"
+                            ><span style="margin-left: 5px">Delete</span></i
+                          >', ['type' => 'submit', 'class' => 'btn btn-xs
+                          btn-danger'] ) }} {!! Form::close() !!}
+                        </td>
+                      </tr>
+                    </tbody>
                     @endforeach
-                  </div>
-                </div>
-
-                <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                  <button type="submit" class="btn btn-primary">Submit</button>
+                  </table>
                 </div>
               </div>
-              {!! Form::close() !!}
             </div>
           </div>
         </div>
---
+++ resources/views/roles/show.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app') @section('content')
+<div class="container text-sm">
+  <div class="row justify-content-center">
+    <div class="col-md">
+      <div class="card">
+        <div class="card-body">
+          <div class="container" style="padding: 1rem">
+            <div class="row">
+              <div class="col">
+                <span style="margin-left: 1rem">
+                  <a href="/roles/create" class="btn btn-success" id="user"
+                    ><b>Create New Role</b></a
+                  >
+                </span>
+                <div class="container pt-3">
+                  <h2>Role :{{$role->name}}</h2>
+
+                  <hr />
+                  <b>Permissions:</b><br />
+                  @foreach ($groupsWithRoles as $post) -{{ $post}}<br />
+                  @endforeach
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
