6308cfe813a909bbdb911fef047dd5baee974dda
Yesho Simon <sepkap123@gmail.com>
2022-05-03 22:59:52 +0300

    roles crud features

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use DB;
+use App\Models\User;
 
 class RoleController extends Controller
 {
@@ -11,9 +15,16 @@ class RoleController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
-        //
+        $roles = Role::all();
+        return view('roles.index')->with('roles',$roles);
+
     }
 
     /**
@@ -23,7 +34,9 @@ class RoleController extends Controller
      */
     public function create()
     {
-        //
+        $permission = Permission::get();
+
+        return view('roles.create',compact('permission'));
     }
 
     /**
@@ -34,7 +47,18 @@ class RoleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+
+            'name' => 'required|unique:roles,name',
+
+            'permission' => 'required',
+
+        ]);
+        $role = Role::create(['name' => $request->input('name')]);
+        $role->syncPermissions($request->input('permission'));
+        return redirect('/roles')->with('success','Role created successfully');
+
+
     }
 
     /**
@@ -45,7 +69,19 @@ class RoleController extends Controller
      */
     public function show($id)
     {
-        //
+        $role = Role::find($id);
+
+        // $rolePermissions = Permission::join("role_has_permissions","role_has_permissions.permission_id","=","permissions.id")
+
+        //     ->where("role_has_permissions.role_id",$id)
+
+        //     ->get();
+            
+            $groupsWithRoles = $role->getPermissionNames();
+            //dd($groupsWithRoles);
+            
+            return view('roles.show',compact('role','groupsWithRoles'));
+
     }
 
     /**
@@ -56,7 +92,20 @@ class RoleController extends Controller
      */
     public function edit($id)
     {
-        //
+        $role = Role::find($id);
+
+        $permission = Permission::get();
+
+        $rolePermissions = DB::table("role_has_permissions")->where("role_has_permissions.role_id",$id)
+
+            ->pluck('role_has_permissions.permission_id','role_has_permissions.permission_id')
+
+            ->all();
+
+    
+
+        return view('roles.edit',compact('role','permission','rolePermissions'));
+
     }
 
     /**
@@ -66,9 +115,20 @@ class RoleController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request,$id)
     {
-        //
+        $this->validate($request, [
+
+            'name' => 'required',
+
+            'permission' => 'required',
+
+        ]);
+        $role = Role::find($id);
+        $role->name = $request->input('name');
+        $role->save();
+        $role->syncPermissions($request->input('permission'));
+        return redirect()->route('roles.index')->with('success','Role updated successfully');
     }
 
     /**
@@ -79,6 +139,8 @@ class RoleController extends Controller
      */
     public function destroy($id)
     {
-        //
+        DB::table("roles")->where('id',$id)->delete();
+        return redirect()->route('roles.index')
+        ->with('success','Role deleted successfully');
     }
 }
