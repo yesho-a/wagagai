8887d8914f4141769423030f9a3bc63c1a1a1c85
Yesho Simon <sepkap123@gmail.com>
2022-05-04 19:35:27 +0300

    added columns to migration tablle

--- database/migrations/2022_05_04_163241_create_categories_table.php
+++ database/migrations/2022_05_04_163241_create_categories_table.php
@@ -15,6 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->integer('parent_id');
             $table->timestamps();
         });
     }
