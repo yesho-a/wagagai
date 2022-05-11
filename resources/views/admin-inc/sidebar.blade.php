a0c141b51cc58018a2dd7ee67a6543c20c065cfb
Yesho Simon <sepkap123@gmail.com>
2022-05-11 16:25:39 +0300

    deleted calender link

--- resources/views/admin-inc/sidebar.blade.php
+++ resources/views/admin-inc/sidebar.blade.php
@@ -230,17 +230,7 @@
               </a>
             </li>
           </ul>
-        </li>
-        <li class="nav-item">
-          <a href="pages/calendar.html" class="nav-link">
-            <i class="nav-icon far fa-calendar-alt"></i>
-            <p>
-              Calendar
-              <span class="badge badge-info right">2</span>
-            </p>
-          </a>
-        </li>
-      
+        </li>  
     </nav>
     <!-- /.sidebar-menu -->
   </div>
