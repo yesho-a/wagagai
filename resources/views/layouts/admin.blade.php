dcb2d70795c0677af0b92706d7b44cb4911706fc
Yesho Simon <sepkap123@gmail.com>
2022-05-11 14:29:14 +0300

    added sidebar

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,11 @@
+<?php include('header.php')?>
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+<div class="wrapper">
+<?php include('navbar.php')?>
+<?php include('sidebar.php')?>
+<?php include('content.php')?>
+<?php include('footer.php')?>
+  <!-- /wrapper -->
+</div>
+</body>
+</html>
