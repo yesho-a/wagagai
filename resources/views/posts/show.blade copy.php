611a75770b330aaee8de5d15b5a15d8e9a398997
Yesho Simon <sepkap123@gmail.com>
2022-05-12 21:50:26 +0300

    changed show post layout

---
+++ resources/views/posts/show.blade copy.php
@@ -0,0 +1,102 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md">
+            <div class="card">
+                <div class="card-body">
+                    <div class="container" style="padding: 1rem;">
+                        <div class="row">
+                             <div class="col">
+                                 <h1 class="text-center pb-3"> <strong>{{$post->post_title}}</strong></h1>
+                             <div class="text-center">
+                                 <img style="width:90%;height:18rem;" src="{{ asset('storage/images/'.$post->image) }} "alt=" {{$post->image}}">
+                                 
+                             </div>
+                             <div class="container pt-4" id="text">
+                                    {{$post->post_body}}
+                                    </div>
+
+                                    <div class="container pt-4" id="text">
+                                        <hr>
+                                        @if ($post->cat==!null)
+                                        <?php
+                                        foreach ($post->cat as $tx) {
+                                        $x = $tx;
+                                        foreach ($cat  as $value) {
+                                          if($value->id == $x){
+                                            echo "<a class='badge' href='/ca/$tx' style='background-color:green;margin-left:3px' name='test'>$value->title</a>";
+        
+                                            echo '<br>';
+                                            }
+                                           }
+                                       }
+        
+        
+                                               ?>
+
+                                        @else
+                                        <p></p>
+                                        @endif
+                                     
+
+                                             <hr>
+                            
+                                             @if (count($post->tags)>=1)
+                                             <h6><strong>Tags</strong></h6>
+                                 
+                                             <?php
+                                             foreach($post->tags as $tag) {
+                                             
+                                             echo "<a class='badge' href='/tags/$tag->name' style='background-color:red;margin-left:3px' name='test'> $tag->name</a>";
+                                            
+     
+                                       
+                                             }
+                                            
+                                               ?> 
+                                             @else
+                                            
+                                             
+                                             @endif
+              
+                                      
+                                    
+                                        </div>
+                                </div>
+                                
+                               
+                                <p  class="text-center"><b>Author: {{ $post->user->name }}<br>
+                                    Published: {{$post->created_at->todatestring()}}</b></p>
+                            </div>
+                            <h4>Display Comments</h4>
+                            @foreach($post->comments as $comment)
+                                <div class="display-comment">
+                                @if($comment->user_id==0)
+                                <strong>Guest</strong>
+                                @else
+                                <strong>{{$comment->user->name }}</strong>
+                                 @endif
+                                    <p>{{ $comment->comment }}</p>
+                                </div>
+                            @endforeach
+                            <hr />
+                            <h4>Add comment</h4>
+                            <form method="post" action="{{ route('comment.add') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <input type="text" name="comment" class="form-control" />
+                                    <input type="hidden" name="post_id" value="{{ $post->id }}" />
+                                </div>
+                                <div class="form-group">
+                                    <input type="submit" class="btn btn-warning" value="Add Comment" />
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,102 +1,47 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md">
-            <div class="card">
-                <div class="card-body">
-                    <div class="container" style="padding: 1rem;">
-                        <div class="row">
-                             <div class="col">
-                                 <h1 class="text-center pb-3"> <strong>{{$post->post_title}}</strong></h1>
-                             <div class="text-center">
-                                 <img style="width:90%;height:18rem;" src="{{ asset('storage/images/'.$post->image) }} "alt=" {{$post->image}}">
-                                 
-                             </div>
-                             <div class="container pt-4" id="text">
-                                    {{$post->post_body}}
-                                    </div>
+<style>
+    .card-body{
+        text-align:left;
+    }
+    
+    body {
+        background-color: rgb(231, 231, 231);
+    }
+    
+      html {
+        background-color: rgb(231, 231, 231);
+    }
+    
+ 
+ h1{
+  font-family: Georgia, sans-serif;
+  font-size: 7em;
+  letter-spacing: -2px;
+}
+</style>
 
-                                    <div class="container pt-4" id="text">
-                                        <hr>
-                                        @if ($post->cat==!null)
-                                        <?php
-                                        foreach ($post->cat as $tx) {
-                                        $x = $tx;
-                                        foreach ($cat  as $value) {
-                                          if($value->id == $x){
-                                            echo "<a class='badge' href='/ca/$tx' style='background-color:green;margin-left:3px' name='test'>$value->title</a>";
-        
-                                            echo '<br>';
-                                            }
-                                           }
-                                       }
-        
-        
-                                               ?>
+ <div class="container d-flex justify-content-center" style= 'margin-bottom: 3rem;'>
+      <div class="card shadow-lg p-4" style="width: 80%">
 
-                                        @else
-                                        <p></p>
-                                        @endif
-                                     
+        <div class="card-body ">
+         <span class="card-title p-4"> <h1  style="font-size:2em">{{$post->post_title}}
+          </h1>
+          <small style="color: lightskyblue">{{$post->created_at->todatestring()}} #  {{ $post->user->name }}</small>
+          </span>
+          
+                    
 
-                                             <hr>
-                            
-                                             @if (count($post->tags)>=1)
-                                             <h6><strong>Tags</strong></h6>
-                                 
-                                             <?php
-                                             foreach($post->tags as $tag) {
-                                             
-                                             echo "<a class='badge' href='/tags/$tag->name' style='background-color:red;margin-left:3px' name='test'> $tag->name</a>";
-                                            
-     
-                                       
-                                             }
-                                            
-                                               ?> 
-                                             @else
-                                            
-                                             
-                                             @endif
-              
-                                      
-                                    
-                                        </div>
-                                </div>
-                                
-                               
-                                <p  class="text-center"><b>Author: {{ $post->user->name }}<br>
-                                    Published: {{$post->created_at->todatestring()}}</b></p>
-                            </div>
-                            <h4>Display Comments</h4>
-                            @foreach($post->comments as $comment)
-                                <div class="display-comment">
-                                @if($comment->user_id==0)
-                                <strong>Guest</strong>
-                                @else
-                                <strong>{{$comment->user->name }}</strong>
-                                 @endif
-                                    <p>{{ $comment->comment }}</p>
-                                </div>
-                            @endforeach
-                            <hr />
-                            <h4>Add comment</h4>
-                            <form method="post" action="{{ route('comment.add') }}">
-                                @csrf
-                                <div class="form-group">
-                                    <input type="text" name="comment" class="form-control" />
-                                    <input type="hidden" name="post_id" value="{{ $post->id }}" />
-                                </div>
-                                <div class="form-group">
-                                    <input type="submit" class="btn btn-warning" value="Add Comment" />
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
+          <p class="card-text" style="text-align:justify">
+            {{$post->post_body}}
+
+ 
+         
         </div>
-    </div>
+        <hr>
+      
+</div>
+</div>
+
 </div>
 @endsection
