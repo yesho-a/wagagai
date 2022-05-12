0ba1aebf99961f6197833b35152da7e5e26ade58
Yesho Simon <sepkap123@gmail.com>
2022-05-12 20:40:38 +0300

    added admin layout to create permission view

--- resources/views/admin-inc/head.blade.php
+++ resources/views/admin-inc/head.blade.php
@@ -6,6 +6,6 @@
   <title>Wagagai Dashboard</title>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-  <link rel="stylesheet" href="css/adminlte.css">
+  <link rel="stylesheet" href="/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="/css/adminlte.css">
 </head>
