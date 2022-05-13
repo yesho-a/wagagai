aab651393998c01671889fee68056c5666fd4b68
Yesho Simon <sepkap123@gmail.com>
2022-05-13 07:36:05 +0300

    change button from submit to publish

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -90,7 +90,7 @@
                                       <input type="text" data-role="tagsinput" class="form-control" name="tags">
                                     </div>
                                   <div class="form-group">
-                                      {{Form::submit('Submit',['class'=>'btn btn-primary'])}}
+                                      {{Form::submit('Publish',['class'=>'btn btn-primary'])}}
                                       {!! Form::close() !!}
                                   </div>
 
