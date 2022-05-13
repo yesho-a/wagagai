4c7a1e2646f7d8a3444366053032e9a0aad273c6
Yesho Simon <sepkap123@gmail.com>
2022-05-13 09:33:34 +0300

    installed diglactic breadcrumbs package

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -22,6 +22,8 @@
 </style>
 
  <div class="container d-flex justify-content-center" style= 'margin-bottom: 3rem;'>
+  {{ Breadcrumbs::render('category', $cat) }}
+
       <div class="card shadow-lg p-4" style="width: 80%">
         <div class="card-body ">
           <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
