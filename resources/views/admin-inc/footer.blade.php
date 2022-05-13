1fa2767cda94d037c34e27c435849f1f9b8a67ea
Yesho Simon <sepkap123@gmail.com>
2022-05-13 17:28:39 +0300

    first version

--- resources/views/admin-inc/footer.blade.php
+++ resources/views/admin-inc/footer.blade.php
@@ -3,7 +3,7 @@
     <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">Tiliatek.com</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.2.0
+      <b>Version</b> 1.0.0
     </div>
   </footer>
 <!-- REQUIRED SCRIPTS -->
