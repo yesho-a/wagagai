f17faa32d4ff287cedf50294e417ebbc63eaef41
Yesho Simon <sepkap123@gmail.com>
2022-05-13 08:03:06 +0300

    add bootrap breadcrumbs

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -23,7 +23,33 @@
 
  <div class="container d-flex justify-content-center" style= 'margin-bottom: 3rem;'>
       <div class="card shadow-lg p-4" style="width: 80%">
+        <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="/">Home</a></li>
+            <li class="breadcrumb-item"><a href="/">Blog</a></li>
+            @if ($post->cat==!null)
+            <?php
+            foreach ($post->cat as $tx) {
+            $x = $tx;
+            foreach ($cat  as $value) {
+              if($value->id == $x){
 
+                echo "<li class='breadcrumb-item'><a href='/ca/$tx'>$value->title</a></li>";
+
+                }
+               }
+           }
+
+
+                   ?>
+
+            @else
+            <p></p>
+            @endif
+         
+
+          </ol>
+        </nav>
         <div class="card-body ">
            
          <span class="card-title p-4"> <h1  style="font-size:2em"><strong>{{$post->post_title}}</strong>
@@ -38,16 +64,18 @@
             
 
           <p class="card-text pt-4" style="text-align:justify">
-            <small class="bg-dark" style="color: lightskyblue">{{$post->created_at->todatestring()}} #  {{ $post->user->name }}</small>
+            <small class="bg-info">{{$post->created_at->todatestring()}} #  {{ $post->user->name }}</small>
             <br>
             {{$post->post_body}}
             <br>
+            <br>
+           <b><i>tags:</i></b>
             @if (count($post->tags)>=1)
 
             <?php
             foreach($post->tags as $tag) {
             
-            echo "<a class='btn badge' href='/tags/$tag->name' style='background-color:red;margin-left:3px' name='test'> $tag->name</a>";
+            echo "<a class='btn badge bg-success' href='/tags/$tag->name' style='margin-left:3px' name='test'> $tag->name</a>";
            
     
       
