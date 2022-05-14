9436fbb4aa88f99ce3fd5b9477c86c4f612331fa
Yesho Simon <sepkap123@gmail.com>
2022-05-14 10:19:41 +0300

    update navbar

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,10 +20,10 @@ class PostController extends Controller
      */
 
 
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
     public function index()
     {
         $posts = Post::all();
--- resources/views/admin-inc/navbar.blade.php
+++ resources/views/admin-inc/navbar.blade.php
@@ -9,6 +9,9 @@
         <a href="/" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
+        <a href="/" class="nav-link">Blog</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
       </li>
     </ul>
