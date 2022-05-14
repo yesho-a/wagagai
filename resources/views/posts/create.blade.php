b05a41e34e555879cdb77b8ffefa53975e195f1d
Yesho Simon <sepkap123@gmail.com>
2022-05-14 11:00:56 +0300

    route authentication

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -39,7 +39,7 @@
                                     <div class="form-group">
                                       <strong>Select Category:</strong>
                                       <br/>
-                                      @foreach($allCategories  as $value)
+                                      @foreach($allCategeories  as $value)
                                       <label>{{ Form::checkbox('cat[]', $value->id, false, array('class' => 'name')) }}
                                         {{ $value->title }}</label>
                                   <br/>
