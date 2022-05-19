8c134413333c429da3176ee26da13a121671d76b
Yesho Simon <sepkap123@gmail.com>
2022-05-19 14:05:51 +0300

    update

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -21,10 +21,10 @@ Breadcrumbs::for('post.index', function (BreadcrumbTrail $trail): void {
 });
 
 // Home > Blog > Post Titles
-Breadcrumbs::for('post.show', function ($trail,$post) {
-    $trail->parent('post.index');
-    $trail->push($post->post_title, route('post.show',$post));
-});
+// Breadcrumbs::for('post.show', function ($trail,$post) {
+//     $trail->parent('post.index');
+//     $trail->push($post->post_title, route('post.show',$post));
+// });
 
 // Home > Blog > Post Titles > Edit
 Breadcrumbs::for('post.edit', function (BreadcrumbTrail $trail, Post $post) {
