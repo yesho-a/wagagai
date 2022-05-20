3b762beb847ef58d7d3020fd0f67233228ddad06
Yesho Simon <sepkap123@gmail.com>
2022-05-20 12:27:04 +0300

    meta model

--- app/Models/Meta.php
+++ app/Models/Meta.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Meta extends Model
 {
     use HasFactory;
+    protected $table = 'metas';
+    public $primaryKey = 'id';
+    public $timestamps = true;
+    protected $fillable = ['meta_title', 'meta_description','meta_keywords'];
 }
