404d5507b9b9546eaa40879cffe4d0f223463cc1
Yesho Simon <sepkap123@gmail.com>
2022-05-20 10:06:05 +0300

    style comments display

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,15 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <li class="nav-item d-none d-sm-inline-block">
+                            <a href="/" class="nav-link">Home</a>
+                          </li>
+                          <li class="nav-item d-none d-sm-inline-block">
+                            <a href="/" class="nav-link">Blog</a>
+                          </li>
+                          <li class="nav-item d-none d-sm-inline-block">
+                            <a href="#" class="nav-link">Contact</a>
+                          </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -63,28 +63,219 @@
   
      
 
-        <hr>
-        <span style="font-family: Cambria, Cochin, Georgia, Times;font-size:1rem"><strong>Comments</strong></span>
-        @foreach($post->comments as $comment)
-            <div class="display-comment mt-1">
-            @if($comment->user_id==0)
-            <strong>Guest</strong>
-            @else
-           {{$comment->user->name }}
-             @endif
-                <p>{{ $comment->comment }}</p>
+ 
+        <style>
+         body{
+    margin-top:20px;
+    background-color:#e9ebee;
+}
+
+.be-comment-block {
+    margin-bottom: 50px !important;
+    border: 1px solid #edeff2;
+    border-radius: 2px;
+    padding: 50px 70px;
+    border:1px solid #ffffff;
+}
+
+.comments-title {
+    font-size: 16px;
+    color: #262626;
+    margin-bottom: 15px;
+    font-family: 'Conv_helveticaneuecyr-bold';
+}
+
+.be-img-comment {
+    width: 60px;
+    height: 60px;
+    float: left;
+    margin-bottom: 15px;
+}
+
+.be-ava-comment {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+}
+
+.be-comment-content {
+    margin-left: 80px;
+}
+
+.be-comment-content span {
+    display: inline-block;
+    width: 49%;
+    margin-bottom: 15px;
+}
+
+.be-comment-name {
+    font-size: 13px;
+    font-family: 'Conv_helveticaneuecyr-bold';
+}
+
+.be-comment-content a {
+    color: #383b43;
+}
+
+.be-comment-content span {
+    display: inline-block;
+    width: 49%;
+    margin-bottom: 15px;
+}
+
+.be-comment-time {
+    text-align: right;
+}
+
+.be-comment-time {
+    font-size: 11px;
+    color: #b4b7c1;
+}
+
+.be-comment-text {
+    font-size: 13px;
+    line-height: 18px;
+    color: #7a8192;
+    display: block;
+    background: #f6f6f7;
+    border: 1px solid #edeff2;
+    padding: 15px 20px 20px 20px;
+}
+
+.form-group.fl_icon .icon {
+    position: absolute;
+    top: 1px;
+    left: 16px;
+    width: 48px;
+    height: 48px;
+    background: #f6f6f7;
+    color: #b5b8c2;
+    text-align: center;
+    line-height: 50px;
+    -webkit-border-top-left-radius: 2px;
+    -webkit-border-bottom-left-radius: 2px;
+    -moz-border-radius-topleft: 2px;
+    -moz-border-radius-bottomleft: 2px;
+    border-top-left-radius: 2px;
+    border-bottom-left-radius: 2px;
+}
+
+.form-group .form-input {
+    font-size: 13px;
+    line-height: 50px;
+    font-weight: 400;
+    color: #b4b7c1;
+    width: 100%;
+    height: 50px;
+    padding-left: 20px;
+    padding-right: 20px;
+    border: 1px solid #edeff2;
+    border-radius: 3px;
+}
+
+.form-group.fl_icon .form-input {
+    padding-left: 70px;
+}
+
+.form-group textarea.form-input {
+    height: 150px;
+}
+
+
+
+
+
+
+          </style>
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+        <div class="container">
+        <div class="be-comment-block">
+          <h1 class="comments-title">Comments (3)</h1>
+          <div class="be-comment">
+            <div class="be-img-comment">	
+              <a href="blog-detail-2.html">
+                <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="" class="be-ava-comment">
+              </a>
             </div>
-        @endforeach
-        <form method="post" action="{{ route('comment.add') }}">
-            @csrf
-            <div class="form-group pb-3">
-                <input type="text" name="comment" class="form-control" placeholder="Add Comment Here" />
-                <input type="hidden" name="post_id" value="{{ $post->id }}" />
+            <div class="be-comment-content">
+              
+                <span class="be-comment-name">
+                  <a href="blog-detail-2.html">Ravi Sah</a>
+                  </span>
+                <span class="be-comment-time">
+                  <i class="fa fa-clock-o"></i>
+                  May 27, 2015 at 3:14am
+                </span>
+        
+              <p class="be-comment-text">
+                Pellentesque gravida tristique ultrices. 
+                Sed blandit varius mauris, vel volutpat urna hendrerit id. 
+                Curabitur rutrum dolor gravida turpis tristique efficitur.
+              </p>
             </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-warning" value="Add Comment" hidden />
+          </div>
+          <div class="be-comment">
+            <div class="be-img-comment">	
+              <a href="blog-detail-2.html">
+                <img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="" class="be-ava-comment">
+              </a>
             </div>
-        </form>
+            <div class="be-comment-content">
+              <span class="be-comment-name">
+                <a href="blog-detail-2.html">Phoenix, the Creative Studio</a>
+              </span>
+              <span class="be-comment-time">
+                <i class="fa fa-clock-o"></i>
+                May 27, 2015 at 3:14am
+              </span>
+              <p class="be-comment-text">
+                Nunc ornare sed dolor sed mattis. In scelerisque dui a arcu mattis, at maximus eros commodo. Cras magna nunc, cursus lobortis luctus at, sollicitudin vel neque. Duis eleifend lorem non ant. Proin ut ornare lectus, vel eleifend est. Fusce hendrerit dui in turpis tristique blandit.
+              </p>
+            </div>
+          </div>
+          <div class="be-comment">
+            <div class="be-img-comment">	
+              <a href="blog-detail-2.html">
+                <img src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="" class="be-ava-comment">
+              </a>
+            </div>
+            <div class="be-comment-content">
+              <span class="be-comment-name">
+                <a href="blog-detail-2.html">Cüneyt ŞEN</a>
+              </span>
+              <span class="be-comment-time">
+                <i class="fa fa-clock-o"></i>
+                May 27, 2015 at 3:14am
+              </span>
+              <p class="be-comment-text">
+                Cras magna nunc, cursus lobortis luctus at, sollicitudin vel neque. Duis eleifend lorem non ant
+              </p>
+            </div>
+          </div>
+          <form class="form-block">
+            <div class="row">
+              <div class="col-xs-12 col-sm-6">
+                <div class="form-group fl_icon">
+                  <div class="icon"><i class="fa fa-user"></i></div>
+                  <input class="form-input" type="text" placeholder="Your name">
+                </div>
+              </div>
+              <div class="col-xs-12 col-sm-6 fl_icon">
+                <div class="form-group fl_icon">
+                  <div class="icon"><i class="fa fa-envelope-o"></i></div>
+                  <input class="form-input" type="text" placeholder="Your email">
+                </div>
+              </div>
+              <div class="col-xs-12">									
+                <div class="form-group">
+                  <textarea class="form-input" required="" placeholder="Your text"></textarea>
+                </div>
+              </div>
+              <a class="btn btn-primary pull-right">submit</a>
+            </div>
+          </form>
+        </div>
+        </div>
       </div>
 
 </div>
